af41b3796db0e2e4474583888f5f541fbcf37b1a
Farahnosh-Yousofi <80102585+Farahnosh-Yousofi@users.noreply.github.com>
2024-09-23 10:04:58 -0400

    First class created

---
+++ class_example.py
@@ -0,0 +1,11 @@
+class User:
+  """Defines a User class"""
+  
+  def __init__(self):
+    """Initializes a new user"""
+    print("Hello from the User class")
+
+
+Farah = User()
+yosuf =  User()
+maria = User()
